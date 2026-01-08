9f2c499c29e800f0863a693b1c1c553d930a59a5
Susheel rai <97819258+mesusheel01@users.noreply.github.com>
2026-01-08 19:34:28 +0530

    Given button type to button element!

--- apps/client/src/components/common-components/SignButton.tsx
+++ apps/client/src/components/common-components/SignButton.tsx
@@ -10,6 +10,7 @@ export const SignButton = ({title, onClick}: {
     const loading = useRecoilValue(loadingAtom)
 
     return <button onClick={onClick}
+            type="submit"
         className={`bg-myGreen border border-black font-play focus:outline-none p-4 py-10 translate-y-3 rounded-xl hover:py-2  transition-all duration-300`}
     >
         {loading ? (title === 'Signin' ? "Signing in..." : "Signing up...") : title}
